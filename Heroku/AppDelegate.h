d8665e681c375be92da006b75847a2fa5c194e17
Aswini Ramesh <aswiniramesh@aswinis-mac.local>
2017-05-15 15:29:04 -0700

    Initial Commit

---
+++ Heroku/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Heroku
+//
+//  Created by Aswini Ramesh on 5/15/17.
+//  Copyright © 2017 Aswini Ramesh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
